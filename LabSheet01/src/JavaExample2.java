5bb7c6aa5685032747ad7fe74881d1285efbc0fd
W-Worapon <cseaza555@gmail.com>
2024-11-27 11:07:07 +0700

    Practice

---
+++ LabSheet01/src/JavaExample2.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class JavaExample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String productName = JOptionPane.showInputDialog("Input Product Name:");
+		String strProductUnit = JOptionPane.showInputDialog("Input Product Unit:");
+		int productUnit = Integer.parseInt(strProductUnit);
+		double productPrice = Double.parseDouble(JOptionPane.showInputDialog(""+"Input Price per Unit:"));
+		double totalPriceofProduct = productUnit * productPrice;
+		double totalwithVat = totalPriceofProduct * 1.07;
+		String frmtotalPriceofProduct = String.format("%,.2f",totalPriceofProduct);
+		String frmtotalwithVat = String.format("%,.2f",totalwithVat);
+		JOptionPane.showMessageDialog(null, "Total Price is "+frmtotalPriceofProduct+" baht."+
+		"\nAdd VAT 7% is "+frmtotalwithVat+ " baht.");
+		
+	}
+
+}
