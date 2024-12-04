73e482ad8aec155b1f1363506cedab1d293e663b
W-Worapon <cseaza555@gmail.com>
2024-12-04 11:22:48 +0700

    LabSheet02onProcessing

---
+++ LabSheet02/src/Lab202.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Lab202 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int count;
+		
+		while(true){
+			System.out.print("Input number : ");
+			int number = input.nextInt();
+			int oldnumber = number;
+			
+			if(number <= oldnumber) {
+				count;
+			}
+		}
+		
+		System.out.println("BYE BYE");
+
+		input.close();
+	}
+
+}
