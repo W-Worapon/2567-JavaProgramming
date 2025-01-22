028998c9ab7e24fc6ec4a33548eb4d2666a6e5ab
W-Worapon <cseaza555@gmail.com>
2025-01-22 08:42:24 +0700

    LabSheet06Homework

---
+++ LabSheet06/src/TestIncomeTaxCalculator.java
@@ -0,0 +1,10 @@
+public class TestIncomeTaxCalculator {
+    public static void main(String[] args) {
+        IncomeTaxCalculator taxCalculator = new IncomeTaxCalculator();
+ 
+        taxCalculator.setIncome(450000);
+        taxCalculator.displayTaxCalculator();
+    }
+ 
+ 
+}
